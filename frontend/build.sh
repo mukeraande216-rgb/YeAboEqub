fb9c3624234e79908045642f210a13379d8c7e81
Daniel Fik <daniunite@outlook.com>
2025-12-26 19:37:30 -0500

    Corrected publish directory and build script

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Install Flutter
-git clone https://github.com/flutter/flutter.git -b stable
+
+# 1. Install Flutter (Stable version)
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Build Web
+# 2. Upgrade and verify
+flutter doctor
+
+# 3. Build the web project
 flutter build web --release
